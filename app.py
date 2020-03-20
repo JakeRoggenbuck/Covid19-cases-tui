1cd88734e9b62d9637de9ba40b0b5965a7a0ba99
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-03-19 17:49:51 -0700

    Convert app.py to use functions

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import requests
-import json
 from termcolor import colored
 
-full_url = f'https://covid2019-api.herokuapp.com/v2/current'
-request = requests.get(full_url)
-json_ = request.json()
+def get_data(url):
+    """Gets a JSON from a url
 
-for item in json_['data']:
+    'url' is any full url string
+    Returns a dictionary
+    """
+    request = requests.get(url)
+    json_ = request.json()
+    
+    return json_['data']
+
+
+for item in get_data(f'https://covid2019-api.herokuapp.com/v2/current'):
     print(f"{colored(item['location'], 'yellow')} Confirmed: {colored(item['confirmed'], 'blue')}, Deaths: {colored(item['deaths'], 'red')}, Recovered: {colored(item['recovered'], 'green')}")
